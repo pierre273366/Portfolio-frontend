da2497bfb2a67099bbf1d98fc4b1948c804f2eb5
Pierre Roitg <pierre.roitg@live.fr>
2024-11-13 17:20:36 +0100

    carroussel

--- script.js
+++ script.js
@@ -84,4 +84,22 @@ function moveReact() {
   }
 }
 
+//Carrouselle:
+
 moveReact();
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 3,
+  speed: 600,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  loop: true,
+});
